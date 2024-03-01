50cd0d5532bc3e506f90c9646df4b2e0c9f86524
ehraazatif <55041932+ehraazatif@users.noreply.github.com>
2024-03-01 18:41:17 -0500

    Create tf_idf.py

---
+++ tf_idf.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def construct_tfidf_1(positional_index: dict, docs: list):
+    words = list(positional_index.keys())
+    num_words = len(words)
+    num_docs = len(docs)
+    matrix = np.zeros((num_words, num_docs))
+    for w in words:
+        values = positional_index[w]
+        
