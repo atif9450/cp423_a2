5df06e67a82c3471b8fb038ae6bd7f605b714346
ehraazatif <55041932+ehraazatif@users.noreply.github.com>
2024-03-02 10:35:15 -0500

    update Q2 files

--- q2.py
+++ q2.py
@@ -1,3 +1,5 @@
-from tf_idf import construct_positional_index
+from tf_idf import construct_positional_index, construct_binary_tfidf
 
 docs, pos_index = construct_positional_index('data/')
+matrix = construct_binary_tfidf(docs, pos_index)
+print(matrix)
--- tf_idf.py
+++ tf_idf.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
+from math import log10
+from copy import deepcopy
 
 def process_file(file):
     text = file.read().lower() #read text and convert to lowercase
@@ -45,5 +47,33 @@ def construct_positional_index(path_to_files):
 def construct_binary_tfidf(doc_indices, positional_index):
     words = list(positional_index.keys()) #get vocabulary
     docs = list(doc_indices.keys()) #get document names
-    matrix = np.array() #initialize TF-IDF matrix
-    
+    matrix = np.zeros((len(words), len(docs))) #initialize TF-IDF matrix
+
+    word_index = 0 #keep track of word index
+    for word in words: #iterate through vocabulary
+        doc_freq = positional_index[word][0] #get document frequency of term
+        idf = log10((len(docs) + 1) / doc_freq) #calculate IDF for term
+        array = positional_index[word][1] #get term frequency array for term
+        vector = np.array([1 if x!=0 else 0 for x in array]) #construct binary vector
+        vector = vector * idf #calculate TF-IDF vector
+        matrix[word_index] = vector #insert vector into matrix
+        word_index += 1 #update word index
+
+    return matrix
+
+
+def construct_raw_count_tfidf(doc_indices, positional_index):
+    words = list(positional_index.keys()) #get vocabulary
+    docs = list(doc_indices.keys()) #get document names
+    matrix = np.zeros((len(words), len(docs))) #initialize TF-IDF matrix
+
+    word_index = 0 #keep track of word index
+    for word in words: #iterate through vocabulary
+        doc_freq = positional_index[word][0] #get document frequency of term
+        idf = log10((len(docs) + 1) / doc_freq) #calculate IDF for term
+        array = positional_index[word][1] #get term frequency array for term
+        vector = array * idf #calculate TF-IDF vector
+        matrix[word_index] = vector #insert vector into matrix
+        word_index += 1 #update word index
+
+    return matrix
