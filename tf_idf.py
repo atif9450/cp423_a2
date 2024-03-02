61a8489ed046fccdd595f0f881ce9a5704c91562
ehraazatif <55041932+ehraazatif@users.noreply.github.com>
2024-03-02 10:36:21 -0500

    Update tf_idf.py

--- tf_idf.py
+++ tf_idf.py
@@ -77,3 +77,19 @@ def construct_raw_count_tfidf(doc_indices, positional_index):
         word_index += 1 #update word index
 
     return matrix
+
+def construct_raw_count_tfidf(doc_indices, positional_index):
+    words = list(positional_index.keys()) #get vocabulary
+    docs = list(doc_indices.keys()) #get document names
+    matrix = np.zeros((len(words), len(docs))) #initialize TF-IDF matrix
+
+    word_index = 0 #keep track of word index
+    for word in words: #iterate through vocabulary
+        doc_freq = positional_index[word][0] #get document frequency of term
+        idf = log10((len(docs) + 1) / doc_freq) #calculate IDF for term
+        array = positional_index[word][1] #get term frequency array for term
+        vector = array * idf #calculate TF-IDF vector
+        matrix[word_index] = vector #insert vector into matrix
+        word_index += 1 #update word index
+
+    return matrix
