18be31c4c9ae244e057ddfc3c48957a6a0fd8442
ehraazatif <55041932+ehraazatif@users.noreply.github.com>
2024-03-02 19:15:43 -0500

    final version for Q2

--- functions.py
+++ functions.py
@@ -82,21 +82,16 @@ def construct_term_freq_tfidf(doc_indices, positional_index):
     matrix = np.zeros((len(words), len(docs))) #initialize TF-IDF matrix
 
     #calculate sum of term frequencies
-    sum_vector = None
-    for word in words: #iterate through vocabulary
-        vector = positional_index[word][1] #get TF vector for term
-        if sum_vector is None:
-            sum_vector = vector
-        else:
-            sum_vector = sum_vector + vector #sum
-    term_freq_sum = sum_vector.sum()
+    raw_matrix = construct_raw_count_tfidf(doc_indices, positional_index) #get raw matrix
+    sum_vector = raw_matrix.sum(axis=0) #get term-frequency sum for each document
+    raw_matrix = None
     
     word_index = 0 #keep track of word index
     for word in words: #iterate through vocabulary
         doc_freq = positional_index[word][0] #get document frequency of term
         idf = log10((len(docs) + 1) / doc_freq) #calculate IDF for term
         array = positional_index[word][1] #get term frequency array for term
-        vector = array / term_freq_sum #calculate term frequency
+        vector = array / sum_vector #calculate term frequency
         vector = vector * idf #calculate TF-IDF vector
         matrix[word_index] = vector #insert vector into matrix
         word_index += 1 #update word index
@@ -126,8 +121,8 @@ def construct_double_norm_tfidf(doc_indices, positional_index):
     docs = list(doc_indices.keys()) #get document names
     matrix = np.zeros((len(words), len(docs))) #initialize TF-IDF matrix
 
-    raw_matrix = construct_raw_count_tfidf(doc_indices, positional_index)
-    max_vector = raw_matrix.max(axis=0)
+    raw_matrix = construct_raw_count_tfidf(doc_indices, positional_index) #get raw matrix
+    max_vector = raw_matrix.max(axis=0) #get max term-frequency for each document
     raw_matrix = None
 
     word_index = 0 #keep track of word index
@@ -141,3 +136,38 @@ def construct_double_norm_tfidf(doc_indices, positional_index):
         word_index += 1 #update word index
 
     return matrix
+
+def cosine_sim(x, y):
+    a = np.dot(x, y)
+    b = np.linalg.norm(x)
+    c = np.linalg.norm(y)
+    sim = a / (b * c)
+    return sim
+
+def get_top_5(query, doc_indices, matrix):
+    sims = {} #keep track of document-query similarities
+    docs = list(doc_indices.keys()) #get list of documents
+    for i in range(len(docs)): #iterate through number of documents
+        doc_vector = matrix[:, i] #get column vector representing document
+        sim = cosine_sim(query, doc_vector.T) #calculate cosine similarity of query and document vectors
+        sims.update({i: sim}) #update similarities dictionary
+
+    sims = dict(sorted(sims.items(), key=lambda item: item[1])) #sort similarities-dictionary on values
+    indices = list(sims.keys()) #get document indices from sorted dictionary
+    indices = indices[:5] #only keep first 5 document indices
+
+    top_5 = [] #collect document names to return
+    doc_names = list(doc_indices.keys()) #get document names
+    for name in doc_names: #iterate through all documents
+        if doc_indices[name] in indices: #if document is among top 5, append to top_5
+            top_5.append(name)
+
+    return top_5
+
+def construct_queries(query, positional_index):
+    words = list(positional_index) #get vocabulary
+    vector = np.array([1 if x in query else 0 for x in words]) #construct bit-vector for query
+    term_freq_vector = vector / vector.sum() #perform term-frequency transformation on query
+    log_norm_vector = np.log10(1 + vector) #perform log normalization on query
+    double_norm_vector = 0.5 + 0.5 * (vector / 1) #perform double normalization on query
+    return vector, vector, term_freq_vector, log_norm_vector, double_norm_vector
--- q2.py
+++ q2.py
@@ -1,6 +1,76 @@
-from tf_idf import *
+from functions import *
+import pickle
 
-docs, pos_index = construct_positional_index('data/')
+docs = None
+pos_index = None
+binary_matrix = None
+raw_count_matrix = None
+term_freq_matrix = None
+log_norm_matrix = None
+double_norm_matrix = None
 
-matrix1 = construct_binary_tfidf(docs, pos_index)
-matrix2 = construct_raw_count_tfidf(docs, pos_index)
+#try to load pre-built files/matrices; construct if failed
+try:
+    docs = pickle.load(open('docs.pkl', 'rb'))
+    pos_index = pickle.load(open('pos_index.pkl', 'rb'))
+    binary_matrix = pickle.load(open('binary.pkl', 'rb'))
+    raw_count_matrix = pickle.load(open('raw_count.pkl', 'rb'))
+    term_freq_matrix = pickle.load(open('term_freq.pkl', 'rb'))
+    log_norm_matrix = pickle.load(open('log_norm.pkl', 'rb'))
+    double_norm_matrix = pickle.load(open('double_norm.pkl', 'rb'))
+except:
+    docs, pos_index = construct_positional_index('data/')
+    binary_matrix = construct_binary_tfidf(docs, pos_index)
+    raw_count_matrix = construct_raw_count_tfidf(docs, pos_index)
+    term_freq_matrix = construct_term_freq_tfidf(docs, pos_index)
+    log_norm_matrix = construct_log_norm_tfidf(docs, pos_index)
+    double_norm_matrix = construct_double_norm_tfidf(docs, pos_index)
+
+    pickle.dump(docs, open('docs.pkl', 'wb'))
+    pickle.dump(pos_index, open('pos_index.pkl', 'wb'))
+    pickle.dump(binary_matrix, open('binary.pkl', 'wb'))
+    pickle.dump(raw_count_matrix, open('raw_count.pkl', 'wb'))
+    pickle.dump(term_freq_matrix, open('term_freq.pkl', 'wb'))
+    pickle.dump(log_norm_matrix, open('log_norm.pkl', 'wb'))
+    pickle.dump(double_norm_matrix, open('double_norm.pkl', 'wb'))
+
+query = input("Please input your query: ") #get user input query
+print('\n')
+with open('query.txt', 'w+') as f: #process query
+    f.write(query)
+    
+with open('query.txt', 'r') as f:
+    query = process_file(f)
+
+#get query vector with different transformations
+binary_vector, raw_count_vector, term_freq_vector, log_norm_vector, double_norm_vector = construct_queries(query, pos_index)
+
+#binary
+top_5 = get_top_5(binary_vector, docs, binary_matrix)
+print("Using binary TF-IDF:")
+print(top_5)
+print('\n')
+
+#raw count
+top_5 = get_top_5(raw_count_vector, docs, raw_count_matrix)
+print("Using raw count TF-IDF:")
+print(top_5)
+print('\n')
+
+#term frequency
+top_5 = get_top_5(term_freq_vector, docs, term_freq_matrix)
+print("Using term frequency TF-IDF:")
+print(top_5)
+print('\n')
+
+#log norm
+top_5 = get_top_5(log_norm_vector, docs, log_norm_matrix)
+print("Using log-normalization TF-IDF:")
+print(top_5)
+print('\n')
+
+#double norm
+top_5 = get_top_5(double_norm_vector, docs, double_norm_matrix)
+print("Using double-normalization TF-IDF:")
+print(top_5)
+print('\n')
